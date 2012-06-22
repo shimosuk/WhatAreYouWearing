2a2d806135f5b65e6b79fe5ac563d27c0975967b
Tagaki Aki <a-tagaki@esm.co.jp>
2012-06-22 23:52:49 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatAreYouWearing::Application.config.secret_token = 'f4e287eb47ffc95ed3717bc09cee7d912830e432040a6a083cdba97288ff8419d071d72a70c7dea7a07f3c8ec24942b7e9b7dfd1f3d04e8371a2cae193240103'
