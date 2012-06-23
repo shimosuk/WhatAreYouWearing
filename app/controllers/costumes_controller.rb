7fe4d214ada45c31383ef3f6b8014a0edf9bcd79
Tagaki Aki <a-tagaki@esm.co.jp>
2012-06-23 14:30:42 +0900

    種類ごとに出し分けたいのでscopeにした

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -11,7 +11,6 @@ class CostumesController < ApplicationController
   end
 
   def index
-    @costumes = Costume.all
     @costume = Costume.new
   end
 end
--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,3 +1,7 @@
 class Costume < ActiveRecord::Base
   attr_accessible :color, :kind
+
+  scope :top, where(kind: 'top')
+  scope :bottom, where(kind: 'bottom')
+  scope :boots, where(kind: 'boots')
 end
