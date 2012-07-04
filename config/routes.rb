a99ad350157f9a70d22048638990519da2f13265
Tagaki Aki <a-tagaki@esm.co.jp>
2012-07-04 15:44:09 +0900

    ルール説明ページを作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 WhatAreYouWearing::Application.routes.draw do
   root to: 'costumes#index'
+
+  match "/costumes/rule" => 'costumes#rule'
   resources :costumes, only: [:index, :show, :create, :destroy] do
     collection do
       get :reset
